bc6975a5f70244a9f25c53032bc9f92a6f3b1321
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2025-08-01 20:12:11 +0530

    env variable fixed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oykoitebvumwpzkupury.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95a29pdGVidnVtd3B6a3VwdXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NzQzNDEsImV4cCI6MjA2OTU1MDM0MX0.y_XMrTXEl2BU6IT4TQ2u7GDpLwpT0MyQpfB30vovh_U";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANNON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
